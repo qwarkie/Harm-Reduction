1a5304f65c5a5f18de5fc273cc9e6f169cff042c
Atlas <dp.mailspace@gmail.com>
2024-10-13 20:31:18 -0600

    Stories section markup + slider setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,54 @@ AOS.init();
 
 $(document).ready(function () {
 
+  const swiper = new Swiper('.stories-slider', {
+    // Pagination with dynamic bullets and clickable functionality
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    
+    // Navigation arrows for next and previous
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // Loop to enable infinite scrolling
+    loop: true,
+  
+    // Coverflow effect
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  
+    // Autoplay configuration
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+
+    speed: 1000,
+  
+    // Responsive breakpoints for different screen sizes
+    breakpoints: {
+      // When window width is >= 320px (mobile)
+      320: {
+        slidesPerView: 1, // Show 1 slide on mobile
+      },
+      // When window width is >= 768px (tablet and above)
+      768: {
+        slidesPerView: 3, // Show 3 slides on tablets and larger screens
+      }
+    }
+  });
+
     // Burger click event
     $(".burger").click(function () {
         $(this).toggleClass("open");
